f7303bbb5a1fcec663f5285ef0eb6c2fb71b9ab2
Lucas Dietrich <ld.adecy@gmail.com>
2021-10-10 23:24:10 +0200

    Fixed timers example

--- src/examples/timers/main.c
+++ src/examples/timers/main.c
@@ -17,7 +17,7 @@ volatile uint16_t counter2 = 0;
 
 void work_handler(struct k_work* work) {
   k_sleep(K_MSEC(200));
-  
+
   usart_print("WORKQUEUE : ");
   usart_u16(counter2);
   usart_transmit('\n');
@@ -37,7 +37,7 @@ void handler2(struct k_timer* timer)
 }
 
 K_TIMER_DEFINE(mytimer1, handler1, K_MSEC(100), 0);
-K_TIMER_DEFINE(mytimer2, handler2, K_MSEC(100), 0);
+K_TIMER_DEFINE(mytimer2, handler2, K_MSEC(100), -1);
 
 int main(void)
 {
