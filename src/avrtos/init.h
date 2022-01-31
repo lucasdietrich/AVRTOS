d96d885bc442f0a125098143e918a8cdb990e5eb
Lucas Dietrich <ld.adecy@gmail.com>
2022-01-31 21:23:54 +0100

    Rename initialization macros

--- src/avrtos/init.h
+++ src/avrtos/init.h
@@ -10,22 +10,24 @@ extern "C" {
 
 /*___________________________________________________________________________*/
 
-#define K_KERNEL_INIT_SP() \
+#define K_KERNEL_LINK_SP_INIT() \
 	__attribute__((naked, used, section(".init3"))) void _k_kernel_sp(void) \
 	{ \
 		extern char _k_main_stack[]; \
 		SP = (uint16_t)_K_STACK_END_ASM(_k_main_stack, THREAD_MAIN_STACK_SIZE); \
 	}
 
-#define K_KERNEL_INIT_AVRTOS() \
+#define K_KERNEL_LINK_AVRTOS_INIT() \
 	__attribute__((naked, used, section(".init8"))) void k_avrtos_init(void) \
 	{ \
 		_k_avrtos_init(); \
 	}
 
-#define K_KERNEL_INIT() \
-	K_KERNEL_INIT_SP() \
-	K_KERNEL_INIT_AVRTOS()
+#define K_KERNEL_LINK_INIT() \
+	K_KERNEL_LINK_SP_INIT() \
+	K_KERNEL_LINK_AVRTOS_INIT()
+
+#define K_KERNEL_INIT K_KERNEL_LINK_INIT
 
 /**
  * @brief Manual user AVRTOS init
