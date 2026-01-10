957735b4832b92e81c2b5f9ddc68cf8cd3b9e295
Lucas Dietrich <lucas.dietrich.git@proton.me>
2026-01-10 23:46:36 +0100

    Remove unnecessary code in uptime example

--- examples/uptime/main.c
+++ examples/uptime/main.c
@@ -11,17 +11,6 @@
 
 #include "avrtos/sys.h"
 
-int timer_handler(struct k_timer *timer)
-{
-    ARG_UNUSED(timer);
-
-    printf("uptime: %lu seconds\n", k_uptime_get());
-
-    return 0;
-}
-
-K_TIMER_DEFINE(mstimer, timer_handler, K_MSEC(1000), 1000);
-
 int main(void)
 {
     led_init();
